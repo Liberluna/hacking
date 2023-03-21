90187e9de6485298411353f4f10f00e117103ae8
nakasyou <nakasyou1103@gmail.com>
2023-03-21 18:41:28 +0900

    add qs

--- src/views/Select.vue
+++ src/views/Select.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
     <h2>Select Stage</h2>
-    <div>
-      
+    <div class="stage-map">
+      <ul v-for="question in questions.questions">
+        <li>
+          {{ question }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 <style scoped>
+.stage-map{
+  overflow-y: scroll;
+  width:100%;
+  height: 100%;
+}
 </style>
 <script lang="ts">
+import questions from "../question/questions.json";
+console.log(questions)
 export default {
   name: "select",
+  data() {
+    return {
+      
+    }
+  },
   methods: {
 
   }
